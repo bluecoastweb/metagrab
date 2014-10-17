017caf6d5860ddf0a2bde2f3d43f4adb5d0834e7
Steve Pedersen <speder@pacbell.net>
2014-10-16 17:33:04 -0700

    added canonical_url

--- metagrab/pi.metagrab.php
+++ metagrab/pi.metagrab.php
@@ -2,7 +2,7 @@
 
 $plugin_info = array(
     'pi_name'        => 'Metagrab',
-    'pi_version'     => '2.0',
+    'pi_version'     => '2.1',
     'pi_author'      => 'Steve Pedersen',
     'pi_author_url'  => 'http://www.bluecoastweb.com',
     'pi_description' => 'Give it up, NSM Better Meta!',
@@ -17,19 +17,24 @@ class Metagrab {
     public function __construct() {
         $this->ee = function_exists('ee') ? ee() : get_instance();
 
-        $entry_id = $this->ee->TMPL->fetch_param('entry_id');
-        if (! $entry_id) {
-            $this->give_up('The "entry_id" parameter is required.');
-        }
-
-        $attribute = $this->ee->TMPL->fetch_param('attribute', 'title');
-        if (! in_array($attribute, array('title', 'description', 'keywords'))) {
-            $this->give_up('The "attribute" parameter must be one: title, description and keywords.');
-        }
+        $this->debug = $this->is_truthy($this->ee->TMPL->fetch_param('debug'));
 
         $default = $this->ee->TMPL->fetch_param('default', '');
 
-        $this->debug = $this->is_truthy($this->ee->TMPL->fetch_param('debug'));
+        $entry_id = $this->ee->TMPL->fetch_param('entry_id');
+        if (! $entry_id) {
+            // no entry, no can do
+            $this->log("No entry_id given, so returning tag default=$default");
+            $this->return_data = $default;
+            return;
+        }
+
+        $attribute = $this->ee->TMPL->fetch_param('attribute', 'title');
+        if (! in_array($attribute, array('title', 'description', 'keywords', 'canonical_url'))) {
+            // not an attribute that we know about
+            $this->give_up('The "attribute" parameter must be one of: title, description, keywords and canonical_url.');
+        }
+
         $this->log("From tag: entry_id=$entry_id, attribute=$attribute, default=$default");
 
         // derive attribute value from NSM Better Meta entry
@@ -42,7 +47,7 @@ class Metagrab {
             $this->log("From NSM: entry_id=$entry_id, $attribute=$value");
         }
 
-        if (empty($value)) {
+        if (empty($value) && $attribute != 'canonical_url') {
             // fall back to NSM Better Meta default setting
             $value = $this->nsm_default($attribute);
             $this->log("From NSM default: $attribute=$value");
@@ -84,15 +89,39 @@ class Metagrab {
         ob_start();
 ?>
 
-{exp:channel:entries limit='1'}
+For a given channel entry id, grab one of the following NSM Better Meta attributes:
+
+* title
+* description
+* keywords
+* canonical_url
+
+and fall back to a given default value if nothing turns up.
+
+For example:
+
+{exp:channel:entries}
 
     {exp:metagrab entry_id='{entry_id}' attribute='title' default='{title}'}
-
     {exp:metagrab entry_id='{entry_id}' attribute='description' default='My Enchanting Description'}
-
     {exp:metagrab entry_id='{entry_id}' attribute='keywords' default='foo bar'}
+    {exp:metagrab entry_id='{entry_id}' attribute='canonical_url' default='{page_url}'}
 
 {/exp:channel:entries}
+
+Or maybe you'd like avoid entering a non-default Canonical URL for every single page on your site:
+
+{exp:switchee variable='{structure:page:entry_id}' parse='inward'}
+
+  {case value=''}
+     <link rel="canonical" href="{current_url}">
+  {/case}
+
+  {case default='yes'}
+     <link rel="canonical" href="{exp:metagrab entry_id='{structure:page:entry_id}' attribute='canonical_url' default='{current_url}'}">
+  {/case}
+
+{/exp:switchee}
 
 <?php
         $buffer = ob_get_contents();
